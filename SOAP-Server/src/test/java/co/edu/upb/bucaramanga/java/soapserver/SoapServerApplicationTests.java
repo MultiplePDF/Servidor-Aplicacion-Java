836ea67087764df499d8e75e4f4364c111b59776
Duvan Diaz <47454957+KillTheReptiles@users.noreply.github.com>
2023-03-26 18:07:19 -0500

    project creation

---
+++ SOAP-Server/src/test/java/co/edu/upb/bucaramanga/java/soapserver/SoapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.upb.bucaramanga.java.soapserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
