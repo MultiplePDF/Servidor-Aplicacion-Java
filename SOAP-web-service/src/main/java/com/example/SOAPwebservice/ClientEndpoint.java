87ef68372210e29cd37574a46be904ae18d21a25
Angela Remolina <angela.remolina.2019@upb.edu.co>
2023-04-09 23:18:42 -0500

    Register operation

--- SOAP-web-service/src/main/java/com/example/SOAPwebservice/ClientEndpoint.java
+++ SOAP-web-service/src/main/java/com/example/SOAPwebservice/ClientEndpoint.java
@@ -2,6 +2,8 @@ package com.example.SOAPwebservice;
 
 import javax.naming.AuthenticationException;
 
+import io.spring.guides.gs_producing_web_service.RegisterRequest;
+import io.spring.guides.gs_producing_web_service.RegisterResponse;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -16,13 +18,16 @@ public class ClientEndpoint {
 
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "loginRequest")
 	@ResponsePayload
-	public LoginResponse login(@RequestPayload LoginRequest request) throws AuthenticationException {
+	public LoginResponse login(@RequestPayload LoginRequest request) {
 	    LoginResponse response = new LoginResponse();
 
-	    String username = request.getUsername();
+	    String email = request.getEmail();
 	    String password = request.getPassword();
 
-	    String token = "hola";
+		// TODO: conexión a la base de datos de Andrey y Yireth através de REST
+		// y comprobar que este usuario esté registrado y las credenciales son correctas
+
+	    String token = "hola login";
 	    if (token != null) {
 	        response.setToken(token);
 	    } else {
@@ -31,5 +36,28 @@ public class ClientEndpoint {
 	    }
 
 	    return response;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "registerRequest")
+	@ResponsePayload
+	public RegisterResponse register(@RequestPayload RegisterRequest request) {
+		RegisterResponse response = new RegisterResponse();
+
+		String name = request.getName();
+		String email = request.getEmail();
+		String password = request.getPassword();
+
+		// TODO: conexión a la base de datos de Andrey y Yireth através de REST
+		// y comprobar registrar el usuario, si el registro es satisfactorio devuelve el token
+
+		String token = "hola register";
+		if (token != null) {
+			response.setToken(token);
+		} else {
+			// If the user is not authenticated, return an error message
+			response.setToken("An error occurred during registration");
+		}
+
+		return response;
 	}
 }
