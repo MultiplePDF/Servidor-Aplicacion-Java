2efe09b97e792721919561ecd082f39a2d4031bf
Angela Remolina <angela.remolina.2019@upb.edu.co>
2023-04-12 10:14:12 -0500

    Merge batch with file endpoint

--- SOAP-web-service/src/main/java/com/example/SOAPwebservice/FilesEndpoint.java
+++ SOAP-web-service/src/main/java/com/example/SOAPwebservice/FilesEndpoint.java
@@ -1,7 +1,6 @@
 package com.example.SOAPwebservice;
 
-import io.spring.guides.gs_producing_web_service.BatchRequest;
-import io.spring.guides.gs_producing_web_service.BatchResponse;
+import io.spring.guides.gs_producing_web_service.*;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -10,10 +9,9 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.Date;
 
 @Endpoint
-public class BatchEndpoint {
+public class FilesEndpoint {
 	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
 
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "sendfileRequest")
@@ -83,6 +81,42 @@ public class BatchEndpoint {
 //	    }
 
 	    return response;
+	}
+
+	// File endpoint methods
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "fileRequest")
+	@ResponsePayload
+	public FileResponse getFileDetails(@RequestPayload FileRequest request) {
+		FileResponse response = new FileResponse();
+
+		String fileID = request.getFileID();
+		String token = request.getToken();
+
+		// TODO: conexión a la base de datos de Yireth y Andrey através de REST
+		// para validar el token, si es valido continua, sino error de autenticación
+
+		// TODO: conexión a la base de datos de Henry, Wilson y Mario através de REST
+		// y devolver toda la información del archivo seleccionado
+
+		// pseudocodigo en comentarios
+
+//	    if (token es válido) {
+//			if (fileID está en la base de datos)
+		response.setFilename("hola.pdf");
+		response.setPath("mulltiplepdf.com/hola.pdf");
+		response.setSize(2.5F);
+//			else{
+		response.setSuccess("File not found");
+//			}
+//	    } else {
+		// If the user is not authenticated, return an error message
+		response.setFilename(null); //probably not needed
+		response.setPath(null); //probably not needed
+		response.setSize(0); //probably not needed
+		response.setSuccess("You session expired, please log in again");
+//	    }
+
+		return response;
 	}
 
 	private String timeFormatter(LocalDateTime time) {
