53913977abe673549f548e8e0fbc58b18e68132f
Angela Remolina <angela.remolina.2019@upb.edu.co>
2023-04-18 11:09:13 -0500

    Update UserEndpoint.java

--- SOAP-web-service/src/main/java/com/example/SOAPwebservice/UserEndpoint.java
+++ SOAP-web-service/src/main/java/com/example/SOAPwebservice/UserEndpoint.java
@@ -9,8 +9,34 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 @Endpoint
 public class UserEndpoint {
 	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+    static final String fakeEmail = "a@gmail.com";
+    static final String fakePass = "123";
+    static final String fakeToken = "abc123";
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "registerRequest")
+    @ResponsePayload
+    public RegisterResponse register(@RequestPayload RegisterRequest request) {
+        RegisterResponse response = new RegisterResponse();
 
-	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "loginRequest")
+        String name = request.getName();
+        String email = request.getEmail();
+        String password = request.getPassword();
+
+        // TODO: conexión a la base de datos de Andrey y Yireth através de REST
+        // y comprobar registrar el usuario, si el registro es satisfactorio devuelve el token
+
+
+        if (fakeToken != null) {
+            response.setToken(fakeToken);
+        } else {
+            // If the user is not authenticated, return an error message
+            response.setToken("An error occurred during registration");
+        }
+
+        return response;
+    }
+
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "loginRequest")
 	@ResponsePayload
 	public LoginResponse login(@RequestPayload LoginRequest request) {
 	    LoginResponse response = new LoginResponse();
@@ -21,38 +47,17 @@ public class UserEndpoint {
 		// TODO: conexión a la base de datos de Andrey y Yireth através de REST
 		// y comprobar que este usuario esté registrado y las credenciales son correctas
 
-	    String token = "hola login";
-	    if (token != null) {
-	        response.setToken(token);
-	    } else {
-	        // If the user is not authenticated, return an error message
-	        response.setToken("Invalid username or password");
-	    }
+        if(email.equals(fakeEmail) && password.equals(fakePass)){
+
+            if (fakeToken != null) {
+                response.setToken(fakeToken);
+            } else {
+                // If the user is not authenticated, return an error message
+                response.setToken("Invalid username or password");
+            }
+        }
 
 	    return response;
-	}
-
-	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "registerRequest")
-	@ResponsePayload
-	public RegisterResponse register(@RequestPayload RegisterRequest request) {
-		RegisterResponse response = new RegisterResponse();
-
-		String name = request.getName();
-		String email = request.getEmail();
-		String password = request.getPassword();
-
-		// TODO: conexión a la base de datos de Andrey y Yireth através de REST
-		// y comprobar registrar el usuario, si el registro es satisfactorio devuelve el token
-
-		String token = "hola register";
-		if (token != null) {
-			response.setToken(token);
-		} else {
-			// If the user is not authenticated, return an error message
-			response.setToken("An error occurred during registration");
-		}
-
-		return response;
 	}
 
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "forgotRequest")
@@ -73,4 +78,40 @@ public class UserEndpoint {
 			response.setSuccessful(false);
 		return response;
 	}
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getUserDetailsRequest")
+    @ResponsePayload
+    public GetUserDetailsResponse getUserDetails(@RequestPayload GetUserDetailsRequest request) {
+        GetUserDetailsResponse response = new GetUserDetailsResponse();
+
+        String email = request.getEmail();
+
+        // TODO: conexión a la base de datos de Andrey y Yireth através de REST
+
+//		pseudo codigo en comentarios:
+//		if(rest == email sent)
+        response.setSuccessful(true);
+//		else
+        response.setSuccessful(false);
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "putUserDetailsRequest")
+    @ResponsePayload
+    public PutUserDetailsResponse putUserDetails(@RequestPayload PutUserDetailsRequest request) {
+        PutUserDetailsResponse response = new PutUserDetailsResponse();
+
+        String email = request.getEmail();
+
+        // TODO: conexión a la base de datos de Andrey y Yireth através de REST
+
+
+//		pseudo codigo en comentarios:
+//		if(rest == email sent)
+        response.setSuccessful(true);
+//		else
+        response.setSuccessful(false);
+        return response;
+    }
+
 }
