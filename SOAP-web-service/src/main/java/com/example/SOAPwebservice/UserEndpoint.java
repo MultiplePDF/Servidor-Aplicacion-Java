9cb284b6ced1f8c9a73e1abc2fc4ec5b7b715b71
Angela Remolina <angela.remolina.2019@upb.edu.co>
2023-04-18 11:20:08 -0500

    fix mistakes

--- SOAP-web-service/src/main/java/com/example/SOAPwebservice/UserEndpoint.java
+++ SOAP-web-service/src/main/java/com/example/SOAPwebservice/UserEndpoint.java
@@ -8,10 +8,12 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class UserEndpoint {
-	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
     static final String fakeEmail = "a@gmail.com";
+    static final String fakeName = "Angela";
     static final String fakePass = "123";
     static final String fakeToken = "abc123";
+
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "registerRequest")
     @ResponsePayload
     public RegisterResponse register(@RequestPayload RegisterRequest request) {
@@ -25,59 +27,56 @@ public class UserEndpoint {
         // y comprobar registrar el usuario, si el registro es satisfactorio devuelve el token
 
 
-        if (fakeToken != null) {
-            response.setToken(fakeToken);
-        } else {
-            // If the user is not authenticated, return an error message
-            response.setToken("An error occurred during registration");
-        }
+        response.setToken(fakeToken);
+        // If the user is not authenticated, return an error message
+        // response.setToken("An error occurred during registration");
+
 
         return response;
     }
 
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "loginRequest")
-	@ResponsePayload
-	public LoginResponse login(@RequestPayload LoginRequest request) {
-	    LoginResponse response = new LoginResponse();
+    @ResponsePayload
+    public LoginResponse login(@RequestPayload LoginRequest request) {
+        LoginResponse response = new LoginResponse();
 
-	    String email = request.getEmail();
-	    String password = request.getPassword();
+        String email = request.getEmail();
+        String password = request.getPassword();
 
-		// TODO: conexión a la base de datos de Andrey y Yireth através de REST
-		// y comprobar que este usuario esté registrado y las credenciales son correctas
+        // TODO: conexión a la base de datos de Andrey y Yireth através de REST
+        // y comprobar que este usuario esté registrado y las credenciales son correctas
 
-        if(email.equals(fakeEmail) && password.equals(fakePass)){
+        if (email.equals(fakeEmail) && password.equals(fakePass)) {
 
-            if (fakeToken != null) {
-                response.setToken(fakeToken);
-            } else {
-                // If the user is not authenticated, return an error message
-                response.setToken("Invalid username or password");
-            }
+
+            response.setToken(fakeToken);
+
+        } else {
+            response.setToken("Invalid username or password");
         }
 
-	    return response;
-	}
+        return response;
+    }
 
-	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "forgotRequest")
-	@ResponsePayload
-	public ForgotResponse forgotPassword(@RequestPayload ForgotRequest request) {
-		ForgotResponse response = new ForgotResponse();
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "forgotRequest")
+    @ResponsePayload
+    public ForgotResponse forgotPassword(@RequestPayload ForgotRequest request) {
+        ForgotResponse response = new ForgotResponse();
 
-		String email = request.getEmail();
+        String email = request.getEmail();
 
-		// TODO: conexión a la base de datos de Andrey y Yireth através de REST
-		// y enviar el email para que se proceda con su recuperación de contraseña
-		// a través del email retornar si fue exitoso o no el envio del correo
+        // TODO: conexión a la base de datos de Andrey y Yireth através de REST
+        // y enviar el email para que se proceda con su recuperación de contraseña
+        // a través del email retornar si fue exitoso o no el envio del correo
 
 //		pseudo codigo en comentarios:
 //		if(rest == email sent)
-			response.setSuccessful(true);
+        response.setSuccessful(true);
 //		else
-			response.setSuccessful(false);
-		return response;
-	}
+        response.setSuccessful(false);
+        return response;
+    }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getUserDetailsRequest")
     @ResponsePayload
@@ -88,11 +87,8 @@ public class UserEndpoint {
 
         // TODO: conexión a la base de datos de Andrey y Yireth através de REST
 
-//		pseudo codigo en comentarios:
-//		if(rest == email sent)
-        response.setSuccessful(true);
-//		else
-        response.setSuccessful(false);
+        response.setEmail(fakeEmail);
+        response.setName(fakeName);
         return response;
     }
 
@@ -105,12 +101,7 @@ public class UserEndpoint {
 
         // TODO: conexión a la base de datos de Andrey y Yireth através de REST
 
-
-//		pseudo codigo en comentarios:
-//		if(rest == email sent)
-        response.setSuccessful(true);
-//		else
-        response.setSuccessful(false);
+        response.setSuccessful("User details succesfully saved");
         return response;
     }
 
