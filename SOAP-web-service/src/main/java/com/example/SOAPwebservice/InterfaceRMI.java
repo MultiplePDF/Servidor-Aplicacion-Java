70f87e0c966ee2a4c367caac5ebd1d4b24cab2e8
Angela Remolina <angela.remolina.2019@upb.edu.co>
2023-04-21 11:04:58 -0500

    Update InterfaceRMI.java

--- SOAP-web-service/src/main/java/com/example/SOAPwebservice/InterfaceRMI.java
+++ SOAP-web-service/src/main/java/com/example/SOAPwebservice/InterfaceRMI.java
@@ -2,9 +2,10 @@ package com.example.SOAPwebservice;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
- 
+
 
 public interface InterfaceRMI extends Remote {
-	public SubBatch conversionOffice(SubBatch objeto) throws RemoteException;
-	public SubBatch conversionURL(SubBatch objeto) throws RemoteException;
+    public SubBatch conversionOffice(SubBatch objeto) throws RemoteException;
+
+    public SubBatch conversionURL(SubBatch objeto) throws RemoteException;
 }
